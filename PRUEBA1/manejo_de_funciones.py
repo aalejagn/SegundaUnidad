706201207df65cd6f82843d3a33c6a58181b6b28
aalejagn <ag0013155@gmail.com>
2025-04-09 11:57:38 -0600

    casi acabamos

---
+++ PRUEBA1/manejo_de_funciones.py
@@ -0,0 +1,33 @@
+from tkinter import Tk, Label,Frame, Entry, Button, ttk, messagebox
+
+"""
+Manejo de interfaz para cada entidad
+"""
+# # Manejo de la entidad de cliente frontend
+def manejar_clientes():
+    messagebox.showinfo("informando", "Hola que hace?")
+    
+# # # # Manejo de la entidad de cliente
+def manejar_inventario():
+    messagebox.showinfo("informando", "Hola que hace?")
+    
+# # Manejo de proveedores
+def manejar_proveedor():
+    messagebox.showinfo("informando", "Hola que hace?")
+    
+# # # Manejo de unidades
+def manejar_unidades():
+    messagebox.showinfo("informando", "Hola que hace?")
+    
+# # Manejo de categorias
+def manejar_categorias():
+    messagebox.showinfo("informando", "Hola que hace?")
+    
+# # # Manejo de metodo de pago
+def manejar_metodo_pago():
+    messagebox.showinfo("informando", "Hola que hace?")
+    
+# # Manejo de empleado
+def manejar_empleado():
+    messagebox.showinfo("informando", "Hola que hace?")
+    
